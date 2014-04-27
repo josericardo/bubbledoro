48ac491990df4010fb3cea39993613ae96f116a2
José Ricardo <zehzinho@gmail.com>
2014-04-27 19:31:33 -0300

    Refactoring the tests, making the pomodoro instantiation explicit
    Will help define other sleepers in future tests.

--- bubbledoro/tests/pomodoro_test.py
+++ bubbledoro/tests/pomodoro_test.py
@@ -6,31 +6,34 @@ WORK_INTERVAL = 25
 SHORT_REST = 5
 LONG_REST = 30
 
+def new_pomodoro(sleeper):
+    return Pomodoro(sleeper, WORK_INTERVAL, SHORT_REST, LONG_REST)
+
 class TestPomodoro(unittest.TestCase):
   def setUp(self):
     self.sleeper = Dingus()
-    self.pomodoro = Pomodoro(self.sleeper,
-                             WORK_INTERVAL,
-                             SHORT_REST,
-                             LONG_REST)
 
   def test_pomodoro_notifies_observers_after_working(self):
     observer = Dingus()
-    self.pomodoro.add_observer(observer)
-    self.pomodoro.work()
+    pomodoro = new_pomodoro(self.sleeper)
+    pomodoro.add_observer(observer)
+    pomodoro.work()
 
     self.assertTrue(observer.wakeup.calls().once)
 
   def test_pomodoro_makes_me_work(self):
-    self.pomodoro.work()
+    pomodoro = new_pomodoro(self.sleeper)
+    pomodoro.work()
     self.slept_for(WORK_INTERVAL)
 
   def test_pomodoro_makes_me_rest_a_little(self):
-    self.pomodoro.short_rest()
+    pomodoro = new_pomodoro(self.sleeper)
+    pomodoro.short_rest()
     self.slept_for(SHORT_REST)
 
   def test_pomodoro_makes_me_rest_longer(self):
-    self.pomodoro.long_rest()
+    pomodoro = new_pomodoro(self.sleeper)
+    pomodoro.long_rest()
     self.slept_for(LONG_REST)
 
   def slept_for(self, interval):

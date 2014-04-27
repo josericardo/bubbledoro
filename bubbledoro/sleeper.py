f78511df1b1d305e347f81b5419021950d75a994
José Ricardo <zehzinho@gmail.com>
2014-04-27 20:08:52 -0300

    The Sleeper can now read user input
    Initially it reads using the unix read command.
    It will have to be improved if other  operating systems get supported.

--- bubbledoro/sleeper.py
+++ bubbledoro/sleeper.py
@@ -1,7 +1,10 @@
 import time
+from commands import getstatusoutput
 
 
 class Sleeper:
     def sleep(self, time_in_min):
-        time.sleep(time_in_min*60)
-        return ''
+        sleep_secs = time_in_min*60
+        cmd = 'read -t %d -n 1 BUBBLEDOROTMP; echo $BUBBLEDOROTMP' % sleep_secs
+        cmd_result = getstatusoutput(cmd)
+        return cmd_result[1]
